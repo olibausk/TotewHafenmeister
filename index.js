cf59251e03c80dcdaa084c5e93e1aef10de583c2
olibausk <plumbum86@gmx.de>
2025-09-27 08:43:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // index.js
+import dotenv from "dotenv";
+dotenv.config();
 import { startBot } from './bot.js';
 import { startAdmin } from './admin.js';
 import { Client, GatewayIntentBits } from "discord.js";
